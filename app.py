6b69bd4bc39205366fa8951797ca7fc60fe757b2
zhshao-312 <zhshao312@gmail.com>
2025-11-18 01:16:05 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-app
+# flask
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
