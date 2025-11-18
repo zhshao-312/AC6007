ac97758c359a03290ff0a7893d11c3e368f10412
zhshao-312 <zhshao312@gmail.com>
2025-11-18 03:22:36 +0000

    1

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
+    r = request.form.get("q")
     return(render_template("index.html"))
 
 @app.route("/main", methods=["GET","POST"])
 def main():
-    r = request.form.get("q")
-    return(render_template("main.html"))
+        return(render_template("main.html"))
 
+@app.route("/dbs", methods=["GET","POST"])
+def dbs():
+        return(render_template("dbs.html"))
 if __name__ == "__main__":
     app.run()
